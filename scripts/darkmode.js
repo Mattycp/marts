5cb5ca62a8b7a69a163ebaa76d0c2885d1978f4c
Mattycp <155780794+Mattycp@users.noreply.github.com>
2024-01-07 20:50:51 -0300

    Add files via upload

--- scripts/darkmode.js
+++ scripts/darkmode.js
@@ -1,30 +1,28 @@
-const changeThemeBtn = document.querySelector("#change-theme")
+const changeThemeBtn = document.querySelector("#change-theme");
 
 // Toggle dark mode
-function toggleDarkMode(){
-    document.body.classList.toggle("dark");
+function toggleDarkMode() {
+  document.body.classList.toggle("dark");
 }
 
-//Load ligth or dark mode
-function loadTheme(){
-    const darkMode = localStorage.getItem("dark")
+// Load light or dark mode
+function loadTheme() {
+  const darkMode = localStorage.getItem("dark");
 
-    if(darkMode){
-        toggleDarkMode();
-    }
-}
-
-loadTheme()
-
-
-changeThemeBtn.addEventListener("change", function() {
+  if (darkMode) {
     toggleDarkMode();
-    
-    // Save or remove dark mode
-    localStorage.removeItem("dark");
+  }
+}
 
-    if (document.body.classList.contains("dark")){
-        localStorage.setItem("dark", 1);
-    }
-})
+loadTheme();
 
+changeThemeBtn.addEventListener("change", function () {
+  toggleDarkMode();
+
+  // Save or remove dark mode from localStorage
+  localStorage.removeItem("dark");
+
+  if (document.body.classList.contains("dark")) {
+    localStorage.setItem("dark", 1);
+  }
+});
---
+++ scripts/scrollreveal.js
@@ -0,0 +1,17 @@
+window.sr = ScrollReveal({reset:true})
+
+ScrollReveal().reveal('.about', {
+    delay: 100
+})
+
+ScrollReveal().reveal('.skills', {
+    delay: 100
+})
+
+ScrollReveal().reveal('.projects', {
+    delay: 100
+})
+
+ScrollReveal().reveal('footer', {
+    delay: 100
+})
